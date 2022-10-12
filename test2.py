9519e5b32808e39a843c299000e6979a1d1218c3
cjavier28 <86753347+cjavier28@users.noreply.github.com>
2022-10-11 21:48:06 -0500

    Primer proyecto

---
+++ test2.py
@@ -0,0 +1,8 @@
+numero1=int(input("primer valor:"))
+numero2=int(input("Segundo valor:"))
+suma=numero1+numero2
+print ("La suma total es"+str(suma))
+
+
+
+
---
+++ test3.py
@@ -0,0 +1,4 @@
+precio=int(input("Ingrese precio "))
+cantidad=int(input("Ingrese cantidad "))
+importe=cantidad*precio
+print(importe)
